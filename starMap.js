89f7ebf7d4deada3e34a26c81adcac297d0fdb95
coderperson1234 <86655188+coderperson1234@users.noreply.github.com>
2022-07-25 21:40:25 +0800

    Create starMap.js

---
+++ starMap.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity} from 'react-native';
+import {createStackNavigator}
+import {WebView} from 'react-native-webview'
+export default function App() {
+  return (
+    
+    
+    );
+}
